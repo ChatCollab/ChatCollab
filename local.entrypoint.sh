5e22f29b8b05adb20fff4d7604afe96030d05cc8
Benjamin Klieger <62450410+Bklieger@users.noreply.github.com>
2024-05-14 15:36:39 -0700

    Create local.entrypoint.sh

---
+++ local.entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+uvicorn chatcollab.timeline.main:app --host 0.0.0.0 --port 8000 & python ./chatcollab/run_slack_source.py & streamlit run "./chatcollab/main.py" --server.port 8080 &
+
+wait
